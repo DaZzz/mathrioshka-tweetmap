b033c011d483b17ca12f8fa4435da8f8e2b1af51
Tikhon Belousko <dazmail01@gmail.com>
2015-11-06 19:44:58 +0200

    Add handles to brush.

--- app/components/tweet-brush.jsx
+++ app/components/tweet-brush.jsx
@@ -13,7 +13,7 @@ let TweetBrush = React.createClass({
 
   componentDidUpdate(nextProps) {
     let shouldAnimateEnter = nextProps.tweets.length != this.props.tweets.length
-    this.componentUpdate(shouldAnimateEnter)  
+    this.componentUpdate(shouldAnimateEnter)
   },
 
   render() {
@@ -25,7 +25,16 @@ let TweetBrush = React.createClass({
 
   initBrush() {
     let container = ReactDOM.findDOMNode(this.refs.brushContainer)
-    let [width, height] = [container.offsetWidth, container.offsetHeight]
+
+    let margin = {
+      top: 20,
+      bottom: 20,
+      left: 20,
+      right: 20
+    }
+
+    let width = container.offsetWidth - margin.left - margin.right
+    let height = container.offsetHeight - margin.top - margin.bottom
 
     let x = d3.time.scale().range([0, width])
     let y = d3.scale.linear().range([height * 0.5, 0])
@@ -40,31 +49,29 @@ let TweetBrush = React.createClass({
 
     // Chart 1
     let chart1 = context.append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
       .append('path')
       .attr('class', 'chart-1')
-      .style('fill', 'rgba(0,0,255,0.2)')
-      .style('stroke', 'blue')
 
-    let chart2 = context.append('g')
+    // Chart 2
+    let chart2 = context
+      .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
       .append('path')
       .attr('class', 'chart-2')
-      .style('fill', 'rgba(255,0,0,0.2)')
-      .style('stroke', 'red')
 
     // Brush
     context.append('g')
       .attr('class', 'brush')
+      .attr('transform', `translate(${margin.left}, 0)`)
 
-    this.setState({context, brush, container, x, y, width, height}, this.onBrush)
+    // Need to call brush after setting params
+    this.setState({context, brush, container, x, y, width, height, margin}, this.onBrush)
   },
 
   onBrush() {
     let { brush, x } = this.state
     let [x1, x2] = brush.extent()
-
-    if (brush.empty()) {
-      
-    }
 
     if (Math.abs(x(x1) - x(x2)) < MIN_BRUSH) {
       x1 = x.invert(Math.min(x.range()[1] - MIN_BRUSH, x(x1)))
@@ -75,7 +82,7 @@ let TweetBrush = React.createClass({
   },
 
   componentUpdate(shouldAnimateEnter) {
-    let {context, brush, container, x, y, width, height} = this.state
+    let {context, brush, container, x, y, width, height, margin} = this.state
     let data = this.props.tweets
     x.domain(d3.extent(data, (d) => d.date))
     y.domain([0, d3.max(data, (d) => d.center)])
@@ -84,13 +91,11 @@ let TweetBrush = React.createClass({
       .x((d) => x(d.date))
       .y1((d) => y(d.center))
       .y0(height * 0.5)
-      .interpolate('cardinal')
 
     let area2 = d3.svg.area()
       .x((d) => x(d.date))
       .y1((d) => height - y(d.periphery))
-      .y0(height * 0.5) 
-      .interpolate('cardinal')
+      .y0(height * 0.5)
 
     let chart1 = context.select('.chart-1')
     let chart2 = context.select('.chart-2')
@@ -110,11 +115,35 @@ let TweetBrush = React.createClass({
       .transition()
       .attr('d', area2)
 
+    let h = height + margin.top + margin.bottom
     context.select('g.brush')
         .call(brush)
       .selectAll('rect')
         .attr('y', 0)
-        .attr('height', height)
+        .attr('height', h)
+
+    let resize = context.select('g.brush')
+        .selectAll('.resize')
+
+    resize.selectAll('.handle')
+        .data([1])
+      .enter()
+        .append('rect')
+        .attr('class', 'handle')
+        .attr('height', h)
+        .attr('width', 2)
+
+    resize.selectAll('.corner')
+        .data([1])
+      .enter()
+        .append('polygon')
+        .attr('class', 'corner')
+
+    context.select('.resize.e .corner')
+        .attr('points', (d, i) => `${0},${h} ${0},${h-10} ${10},${h}`)
+
+    context.select('.resize.w .corner')
+        .attr('points', (d, i) => `${0},${h} ${0},${h-10} ${-10},${h}`)
   },
 })
 
